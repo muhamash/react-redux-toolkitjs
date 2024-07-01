59fd7b4c22b467bfcce8d4b9ab67ce3650797b56
muhamash <muhammad-ashraful@outlook.com>
2024-07-01 18:27:14 +0600

    redux works

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
 /* eslint-disable no-undef */
-import React from 'react';
-import { useSelector } from 'react-redux';
+// import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import Counter from './components/Counter';
 import Stats from './components/Stats';
+import { decrement, increment } from './features/counters/counterSlice';
 
 
 function App() {
 
-  const state = useSelector((state => state.counter))
-  const handleIncrement = ( id ) =>
-  {
-    setState( ( prev ) =>
-      prev.map( ( count ) => count.id === id ? { ...count, value: count.value + 1 } : count )
-    )
-  };
+  const state = useSelector( ( state => state.counter ) )
+  
+  const dispatch = useDispatch()
+  // const handleIncrement = ( id ) =>
+  // {
+  //   setState( ( prev ) =>
+  //     prev.map( ( count ) => count.id === id ? { ...count, value: count.value + 1 } : count )
+  //   )
+  // };
 
-  const handleDecrement = ( id ) =>
-  {
-    setState( ( prev ) =>
-      prev.map( ( count ) => count.id === id ? { ...count, value: count.value - 1 } : count )
-    )
-  };
+  // const handleDecrement = ( id ) =>
+  // {
+  //   setState( ( prev ) =>
+  //     prev.map( ( count ) => count.id === id ? { ...count, value: count.value - 1 } : count )
+  //   )
+  // };
 
   const total = state.reduce((acc, sum)=> acc + sum.value, 0)
 
@@ -33,8 +36,8 @@ function App() {
             <Counter
               key={ count.id }
               count={ count.value }
-              onIncrement={ () => handleIncrement(count.id) }
-              onDecrement={ () => handleDecrement(count.id) }
+              onIncrement={ () => dispatch(increment(count.id)) }
+              onDecrement={ () => dispatch(decrement(count.id)) }
             />
           ) )
         }
